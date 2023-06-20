aea9f1c39c4c3974cd1d0cac8dc14dbb3a934353
Adrian Hodos <adi.hodos@gmail.com>
2023-06-20 16:07:45 +0300

    Orthographic projection matrix for Vulkan coordinate system.

--- src/projection.rs
+++ src/projection.rs
@@ -1,57 +1,62 @@
-use super::mat4::Mat4F32;
-use std::convert::From;
+pub mod vk {
+    use crate::mat4::Mat4F32;
+    use std::convert::From;
 
-#[rustfmt::skip]
-pub fn orthographic(left: f32, top: f32, right: f32, bottom: f32, near: f32, far: f32) -> Mat4F32 {
-    let width = right - left;
-    let height = top - bottom;
+    /// Orthographic projection matrix with reversed depth (1 -> 0), for Vulkan coordinate system.
+    pub fn ortho(left: f32, right: f32, top: f32, bottom: f32, near: f32, far: f32) -> Mat4F32 {
+        Mat4F32 {
+            a00: 2f32 / (right - left),
+            a01: 0f32,
+            a02: 0f32,
+            a03: -(right + left) / (right - left),
 
-    Mat4F32::from(
-    [
-        2_f32 / width,
-        0_f32,
-        0_f32,
-        -(right + left) / width,
+            //
+            //
+            a10: 0f32,
+            a11: 2f32 / (bottom - top),
+            a12: 0f32,
+            a13: -(top + bottom) / (bottom - top),
 
-        0_f32,
-        2_f32 / height,
-        0_f32,
-        -(top + bottom) / height,
+            //
+            //
+            a20: 0f32,
+            a21: 0f32,
+            a22: 1f32 / (near - far),
+            a23: -far / (near - far),
 
-        0_f32,
-        0_f32,
-        -2_f32 / (far - near),
-        -(far + near) / (far - near),
+            //
+            //
+            a30: 0f32,
+            a31: 0f32,
+            a32: 0f32,
+            a33: 1f32,
+        }
+    }
 
-        0_f32,
-        0_f32,
-        0_f32,
-        1_f32,
-    ])
-}
+    /// Orthographic projection matrix with reversed depth (1 -> 0), for Vulkan coordinate system.
+    #[rustfmt::skip]
+    pub fn ortho_symmetric(right: f32, top: f32, near: f32, far: f32) -> Mat4F32 {
+        Mat4F32::from(
+        [
+            2_f32 / right,
+            0_f32,
+            0_f32,
+            -1_f32,
 
-#[rustfmt::skip]
-pub fn orthographic_symmetric(right: f32, top: f32, near: f32, far: f32) -> Mat4F32 {
-    Mat4F32::from(
-    [
-        1_f32 / right,
-        0_f32,
-        0_f32,
-        0_f32,
+            0_f32,
+            2_f32 / top,
+            0_f32,
+            -1_f32,
 
-        0_f32,
-        1_f32 / top,
-        0_f32,
-        0_f32,
+            0_f32,
+            0_f32,
+            1f32 / (near - far),
+            -far / (near - far),
 
-        0_f32,
-        0_f32,
-        -2_f32 / (far - near),
-        -(far + near) / (far - near),
-        
-        0_f32,
-        0_f32,
-        0_f32,
-        1_f32,
-    ])
+            0_f32,
+            0_f32,
+            0_f32,
+            1_f32,
+        ])
+    }
 }
